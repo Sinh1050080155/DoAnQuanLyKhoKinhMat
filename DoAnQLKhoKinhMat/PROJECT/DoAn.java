c0ccf30c796f9f019b5b28ebd1b7c9918e72a242
MOI <125429145+Sinh1050080155@users.noreply.github.com>
2023-04-16 08:31:57 +0700

    Add files via upload

---
+++ DoAnQLKhoKinhMat/PROJECT/DoAn.java
@@ -0,0 +1,136 @@
+package PROJECT;
+import java.util.Scanner;
+
+public class DoAn
+{
+	public static void main(String[] args)
+	{
+		QuanLy a = new QuanLy();
+		KinhMat b;
+		Phieu c;
+		int chon;
+		Scanner sc = new Scanner(System.in);
+		a.menuChinh();
+		do
+		{
+			System.out.print("\nNhap Lua Chon: "); chon = sc.nextInt();
+			switch (chon)
+			{
+                                case 1:
+				{
+					c = new PhieuNhapKho();
+					a.themDSPhieu(c);
+				} break;
+                                case 2:
+				{
+					c = new PhieuXuatKho();
+					a.themDSPhieu(c);
+				} break;
+                                case 3:
+				{
+					a.hienDanhSachPhieuNhapKho();
+				} break;
+                                case 4:
+				{
+					a.hienDanhSachPhieuXuatKho();
+				} break;
+                                case 5:
+                                {
+                                        a.hienDanhSachPhieu();
+                                } break;
+                                case 6:
+				{
+					a.SuaDoiPhieu();
+				} break;
+                                case 7:
+				{
+					a.sapXepTheoSoPhieu();
+				} break;
+                                case 8:
+				{
+					a.hienPhieuTenKhachHang_MINH();
+				} break;
+                                case 9:
+				{
+					String tenFile;
+					System.out.print("\nNhap Ten File Muon Luu: "); sc.nextLine();
+					tenFile = sc.nextLine();
+					a.ghiFileP(tenFile);
+				} break;
+                                case 10:
+				{
+					String tenFile;
+					System.out.print("Nhap Ten File Muon Doc: "); sc.nextLine();
+					tenFile = sc.nextLine();
+					a.docFileP(tenFile);
+				} break;
+                                case 11:
+				{
+					b = new KinhThuoc();
+					a.themDSKinhMat(b);
+				} break;
+                                case 12:
+				{
+					b = new KinhBaoHo();
+					a.themDSKinhMat(b);
+				} break;
+                                case 13:
+				{
+					b = new KinhThoiTrang();
+					a.themDSKinhMat(b);
+				} break;
+                                case 14:
+				{
+					b = new KinhRam();
+					a.themDSKinhMat(b);
+				} break;
+                                case 15:
+				{
+					a.hienDanhSachKinhThuoc();
+				} break;
+                                case 16:
+				{
+					a.hienDanhSachKinhBaoHo();
+				} break;
+                                case 17:
+				{
+					a.hienDanhSachKinhThoiTrang();
+				} break;
+                                case 18:
+				{
+					a.hienDanhSachKinhRam();
+				} break;
+                                case 19:
+                                {
+                                        a.hienDanhSachKinhMat();
+                                }
+                                case 20:
+				{
+					a.SuaDoiKM();
+				} break;
+                                case 21:
+                                {
+                                        a.sapXepTheoSoLuongKinhMat();
+                                } break;
+                                case 22:
+                                {
+                                        a.hienKinhMatNguonGoc_USA();
+                                } break;
+                                case 23:
+				{
+					String tenFile;
+					System.out.print("\nNhap Ten File Muon Luu: "); sc.nextLine();
+					tenFile = sc.nextLine();
+					a.ghiFileKM(tenFile);
+				} break;
+                                case 24:
+				{
+					String tenFile;
+					System.out.print("Nhap Ten File Muon Doc: "); sc.nextLine();
+					tenFile = sc.nextLine();
+					a.docFileKM(tenFile);
+				} break;
+			}
+		} while(chon != 0);
+	}
+}
---
+++ DoAnQLKhoKinhMat/PROJECT/KinhMat.java
@@ -0,0 +1,122 @@
+package PROJECT;
+import java.util.Scanner;
+import java.io.Serializable;
+
+public class KinhMat implements Serializable
+{
+	private String tenTH;
+	private String tenSP;
+	private String maSP;
+	private String desCription;
+	private int soLuong;
+	private long giaSP;
+	private String nguonGoc;
+
+	public KinhMat()
+	{
+	}
+
+	public KinhMat(String tenTH, String tenSP, String maSP, String desCription, int soLuong, long giaSP, String nguonGoc)
+	{
+		this.tenTH = tenTH;
+		this.tenSP = tenSP;
+		this.maSP = maSP;
+		this.desCription = desCription;
+		this.soLuong = soLuong;
+		this.giaSP = giaSP;
+		this.nguonGoc = nguonGoc;
+	}
+
+	public void setTenThuongHieu(String tenTH)
+	{
+		this.tenTH = tenTH;
+	}
+
+	public String getTenThuongHieu()
+	{
+		return this.tenTH;
+	}
+
+	public void setTenSanPham(String tenSP)
+	{
+		this.tenSP = tenSP;
+	}
+
+	public String getTenSanPham()
+	{
+		return this.tenSP = tenSP;
+	}
+
+	public void setMaSanPham(String maSP)
+	{
+		this.maSP = maSP;
+	}
+
+	public String getMaSanPham()
+	{
+		return this.maSP;
+	}
+
+	public void setDescription(String desCription)
+	{
+		this.desCription = desCription;
+	}
+
+	public String getDescription()
+	{
+		return this.desCription;
+	}
+
+	public void setSoLuong(int soLuong)
+	{
+		this.soLuong = soLuong;
+	}
+
+	public int getSoLuong()
+	{
+		return this.soLuong;
+	}
+
+	public void setGiaSanPham(long giaSP)
+	{
+		this.giaSP = giaSP;
+	}
+
+	public long getGiaSanPham()
+	{
+		return this.giaSP;
+	}
+
+	public void setNguonGoc(String nguonGoc)
+	{
+		this.nguonGoc = nguonGoc;
+	}
+
+	public String getNguonGoc()
+	{
+		return this.nguonGoc;
+	}
+
+	public void nhapThongTin()
+	{
+		Scanner kinhmat = new Scanner(System.in);
+		System.out.print("Nhap Ten Thuong Hieu: "); tenTH = kinhmat.nextLine();
+		System.out.print("Nhap Ten San Pham: "); tenSP = kinhmat.nextLine();
+		System.out.print("Nhap Ma San Pham: "); maSP = kinhmat.nextLine();
+		System.out.print("Nhap Mo Ta San Pham: "); desCription = kinhmat.nextLine();
+		System.out.print("Nhap So Luong San Pham: "); soLuong = kinhmat.nextInt();
+		System.out.print("Nhap Gia San Pham: "); giaSP = kinhmat.nextLong(); kinhmat.nextLine();
+		System.out.print("Nhap Nguon Goc San Pham: "); nguonGoc = kinhmat.nextLine();
+	}
+
+	public void hienThongTin()
+	{
+		System.out.println("\nTen Thuong Hieu: " + tenTH);
+		System.out.println("Ten San Pham: " + tenSP);
+		System.out.println("Ma San Pham: " + maSP);
+		System.out.println("Mo Ta San Pham: " + desCription);
+		System.out.println("So luong San Pham: " + soLuong);
+		System.out.println("Gia San Pham: " + giaSP);
+		System.out.println("Nguon Goc San Pham: " + nguonGoc);
+	}
+}
